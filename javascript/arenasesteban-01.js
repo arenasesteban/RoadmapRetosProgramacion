32e5ad55d4822d95d343eef81c5ee14586a5bd10
arenasesteban <esteban.arenas.a@usach.cl>
2024-06-17 00:49:05 -0400

    [Esteban] Reto #01 - Operadores y estructuras de control

--- javascript/arenasesteban-01.js
+++ javascript/arenasesteban-01.js
@@ -171,6 +171,7 @@ console.log("For In         - ", respuesta);
 console.log("");
 
 // [ 4 ] - Crea un programa que imprima por consola todos los números comprendidos entre 10 y 55 (incluidos), pares, y que no son ni el 16 ni múltiplos de 3.
+
 console.log("--+ DIFICULTAD EXTRA +--\n");
 console.log("Números comprendidos entre 10 y 55 (incluidos), pares y multiplos de 3, menos el 16.")
 for(let i = 10; i <= 55; i++) {  
